2665574f44dba0518a0ec758be0f6beb83e05d93
MeltedGreenVelvet <89157503+MeltedGreenVelvet@users.noreply.github.com>
2023-12-26 11:48:29 -0500

    Created validate.js
    - Added script to handle form validation.

---
+++ validate.js
@@ -0,0 +1,39 @@
+const emailCollector = document.getElementById("email-collector");
+const email = document.getElementById("email");
+
+emailCollector.addEventListener('submit', e => {
+    e.preventDefault();
+    validateEmail();
+})
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateEmail = () => {
+    const emailValue = email.value.trim();
+    if (emailValue === '' || !isValidEmail(emailValue)) {
+        setError(email, 'Please provide a valid email');
+    } else {
+        setSuccess(email);
+        let successDisplay = document.getElementById("successMessage");
+        successDisplay.textContent += "Thank you for subscribing to our newsletter.";
+    }
+}
