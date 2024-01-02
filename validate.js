a8426ee9390efab63d8fbaa1f30ed6adcaefd677
MeltedGreenVelvet <89157503+MeltedGreenVelvet@users.noreply.github.com>
2024-01-01 19:56:04 -0500

    Added error icon
    - Modified js script to create svg
    - Added error icon styling (absolute positioning)

--- validate.js
+++ validate.js
@@ -14,6 +14,7 @@ const setError = (element, message) => {
     inputControl.classList.add('errorColor');
     inputControl.classList.remove('success');
     inputControl.classList.remove('successColor');
+    addErrorImage(inputControl);
 }
 
 const setSuccess = element => {
@@ -24,12 +25,29 @@ const setSuccess = element => {
     inputControl.classList.add('successColor'); // Add successColor class to change the color
     inputControl.classList.remove('error');
     inputControl.classList.remove('errorColor');
+    removeErrorImage(inputControl);
 };
 
 const isValidEmail = email => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+const addErrorImage = (element) => {
+    const img = document.createElement('img');
+    img.src = 'images/icon-error.svg'; 
+    img.alt = 'Validation Error';
+    img.classList.add('icon-error');
+    element.appendChild(img);
+};
+
+const removeErrorImage = (element) => {
+    const errorImage = element.querySelector('.icon-error');
+    if (errorImage) {
+        element.removeChild(errorImage);
+    }
+};
+
 
 const validateEmail = () => {
     const emailValue = email.value.trim();
